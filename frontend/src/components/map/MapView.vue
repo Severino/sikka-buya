ef8f0da7d0505f8d161a9fd5d793a6412a70653a
Severino <severin.opel@outlook.com>
2021-10-01 15:56:35 +0200

    Fixed map.

--- frontend/src/components/map/MapView.vue
+++ frontend/src/components/map/MapView.vue
@@ -74,11 +74,14 @@ export default {
       map.invalidateSize();
     }
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      {
+        maxZoom: 17,
+        attribution:
+          'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+      }
+    ).addTo(map);
 
     this.$data.map = map;
     this.ready = true;
