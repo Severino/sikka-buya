afa4582421341cf8b0f1ee3bfbcb090d73922930
Severino <severin.opel@outlook.com>
2021-11-04 16:14:33 +0100

    Added small type preview.

--- frontend/src/components/page/catalog/PersonExplorer.vue
+++ frontend/src/components/page/catalog/PersonExplorer.vue
@@ -4,10 +4,11 @@
       <div class="list">
         <collapsible
           v-for="person of persons"
+          :class="{ highlight: person.id == 8 }"
           :key="person.id"
           @open="getTypesByPerson(person)"
         >
-          <template slot="header">{{ person.name }} </template>
+          <template slot="header">{{ person.name }}</template>
           <div v-if="map[person.id]">
             <p
               v-if="
@@ -19,10 +20,64 @@
             </p>
 
             <collapsible
-              v-for="(mint, idx) of Object.values(map[person.id])"
-              :key="'mint-' + person.id + '-' + mint.value.name + '-' + idx"
+              v-for="mintObject of Object.values(map[person.id])"
+              :key="'mint-' + person.id + '-' + mintObject.value.name"
+              class="year-grid"
             >
-              <template slot="header">{{ mint.value.name }}</template>
+              <template slot="header">{{ mintObject.value.name }}</template>
+              <collapsible
+                v-for="timeObject of mintObject.children"
+                :key="
+                  'mint-' +
+                  person.id +
+                  '-' +
+                  mintObject.value.name +
+                  '-' +
+                  timeObject.value
+                "
+              >
+                <template slot="header">{{ timeObject.value }}</template>
+                <collapsible
+                  v-for="type of timeObject.children"
+                  :key="
+                    'mint-' +
+                    person.id +
+                    '-' +
+                    mintObject.value.name +
+                    '-' +
+                    timeObject.value +
+                    '-' +
+                    type.id
+                  "
+                >
+                  <template slot="header">
+                    {{ type.projectId }}
+                  </template>
+
+                  <div class="grid">
+                    <labeled-property label="Material">
+                      {{ type.material.name }}
+                    </labeled-property>
+                    <labeled-property label="Donativ">
+                      {{ type.donativ }}
+                    </labeled-property>
+                    <labeled-property label="Herstellungsart">
+                      {{ type.procedure }}
+                    </labeled-property>
+                  </div>
+                  <labeled-property label="Avers">
+                    <div v-html="type.avers.fieldText" />
+                    <b>Umschriften</b>
+                    <ol>
+                      <li
+                        v-for="(inscript, idx) of getInscripts(type.avers)"
+                        :key="idx"
+                        v-html="inscript"
+                      />
+                    </ol>
+                  </labeled-property>
+                </collapsible>
+              </collapsible>
             </collapsible>
           </div>
         </collapsible>
@@ -33,9 +88,10 @@
 
 <script>
 import Query from '../../../database/query';
+import LabeledProperty from '../../display/LabeledProperty.vue';
 import Collapsible from '../../layout/Collapsible.vue';
 export default {
-  components: { Collapsible },
+  components: { Collapsible, LabeledProperty },
   data: function () {
     return {
       persons: [],
@@ -65,8 +121,30 @@ export default {
         getTypesByOverlord(id:${person.id})
         {
           id
+          projectId
           mint {id name}
           yearOfMint
+          material {name}
+          nominal {name}
+          donativ
+          procedure
+          issuers {shortName}
+          overlords {shortName}
+          caliph {shortName}
+          avers {
+              fieldText
+              innerInscript
+              intermediateInscript
+              outerInscript
+              misc
+          }
+          reverse {
+              fieldText
+              innerInscript
+              intermediateInscript
+              outerInscript
+              misc
+          }
         }
         }`
         )
@@ -75,15 +153,22 @@ export default {
             this.types = types;
 
             let mints = {};
-            console.log(types);
             types.forEach((type) => {
-              console.log(type);
               if (type?.mint?.id) {
                 const mintId = type.mint.id;
                 if (!mints[mintId])
-                  mints[mintId] = { value: type.mint, children: [] };
+                  mints[mintId] = { value: type.mint, children: {} };
 
-                mints[mintId].children.push(type);
+                if (type.yearOfMint) {
+                  if (!mints[mintId].children[type.yearOfMint]) {
+                    mints[mintId].children[type.yearOfMint] = {
+                      value: type.yearOfMint,
+                      children: [],
+                    };
+                  }
+
+                  mints[mintId].children[type.yearOfMint].children.push(type);
+                } else console.error('Type has no year set.');
               }
             });
 
@@ -92,9 +177,54 @@ export default {
           .catch(console.error);
       }
     },
+    getInscripts(coinside) {
+      function hasContent(htmlString) {
+        try {
+          const parser = new DOMParser();
+          let document = parser.parseFromString(htmlString, 'text/html');
+          return document.body.textContent == '' ? false : true;
+        } catch (e) {
+          console.error(e);
+        }
+
+        return false;
+      }
+
+      let inscripts = [];
+      ['innerInscript', 'intermediateInscript', 'outerInscript'].forEach(
+        (prop) => {
+          if (coinside[prop] && hasContent(coinside[prop])) {
+            inscripts.push(coinside[prop]);
+          }
+        }
+      );
+
+      return inscripts;
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.highlight header {
+  color: white;
+  background-color: $primary-color !important;
+}
+
+.year-grid > .list-filter-container-content {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  align-items: start;
+
+  // .list-filter-container-content {
+  //   background-color: red;
+  //   align-self: start;
+  // }
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
 </style>
