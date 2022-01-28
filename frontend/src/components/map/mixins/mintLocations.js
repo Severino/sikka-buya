30fd77a622e3eae08bbe54d790bac619c0420d23
Severino <severin.opel@outlook.com>
2022-01-28 15:08:46 +0100

    Reimplemented bringtofront

--- frontend/src/components/map/mixins/mintLocations.js
+++ frontend/src/components/map/mixins/mintLocations.js
@@ -1,7 +1,7 @@
 export default {
     updateMintLocationMarker() {
         const _ = new MintLocation(this);
-        _.removeExistingLocation();
+        // _.removeExistingLocation();
         let features = _.mapToGeoJsonFeature(this.mints);
         this.mintLocations = _.createGeometryLayer(features);
         this.mintLocations.addTo(this.map);
