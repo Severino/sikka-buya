0bf869e4eeb131ad025c52e69a691ea22bfccc55
Severino <severin.opel@outlook.com>
2022-02-11 11:40:59 +0100

    Fixed year mint table scaled scrolling

--- frontend/src/components/page/analytics/YearMintTablePage.vue
+++ frontend/src/components/page/analytics/YearMintTablePage.vue
@@ -1,62 +1,92 @@
 <template>
   <div class="page">
-    <h1>
-      <select @input="xChanged">
-        <option
-          v-for="(val, index) of values"
-          :value="val"
-          :key="index"
-          :selected="val == x"
-        >
-          {{ val }}
-        </option>
-      </select>
-      /
-      <select @input="yChanged">
-        <option
-          v-for="(val, index) of values"
-          :value="val"
-          :key="index"
-          :selected="val == y"
-        >
-          {{ val }}
-        </option>
-      </select>
-    </h1>
+    <header>
+      <h2>Analytics Table</h2>
+
+      <div class="select-row">
+        <labeled-property label="X-Achse">
+          <select @input="xChanged">
+            <option
+              v-for="(val, index) of values"
+              :value="val"
+              :key="index"
+              :selected="val == x"
+            >
+              {{ val }}
+            </option>
+          </select>
+        </labeled-property>
+        <labeled-property label="Y-Achse">
+          <select @input="yChanged">
+            <option
+              v-for="(val, index) of values"
+              :value="val"
+              :key="index"
+              :selected="val == y"
+            >
+              {{ val }}
+            </option>
+          </select>
+        </labeled-property>
+        <labeled-property label="Skalierung">
+          <slider
+            :min="0"
+            :max="1"
+            :value="scale"
+            @input="updateScale"
+            :step="0.01"
+          />
+        </labeled-property>
+      </div>
+    </header>
     <div v-if="error" class="error"></div>
-    <table>
-      <thead>
-        <tr>
-          <td></td>
-          <td v-for="(itemX, xIdx) of xValues" :key="'row-' + xIdx">
-            {{ itemX }}
-          </td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(itemY, yIdx) in yValues" :key="'head-' + yIdx">
-          <td>{{ itemY }}</td>
-          <td
-            v-for="(itemX, xIdx) of xValues"
-            v-bind:key="'cell-' + yIdx + '-' + xIdx"
-            class="color-box"
-            :class="{ exists: getTypesFromMap(itemX, itemY).length != 0 }"
-          >
-            {{ getTypesFromMap(itemX, itemY).length }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+
+    <div
+      class="viewport"
+      ref="viewport"
+      @scroll.passive="pinTableHeaders($event)"
+    >
+      <table ref="table">
+        <thead>
+          <tr>
+            <td></td>
+            <td v-for="(itemX, xIdx) of xValues" :key="'row-' + xIdx">
+              {{ itemX }}
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(itemY, yIdx) in yValues" :key="'head-' + yIdx">
+            <td>{{ itemY }}</td>
+            <td
+              v-for="(itemX, xIdx) of xValues"
+              v-bind:key="'cell-' + yIdx + '-' + xIdx"
+              class="color-box"
+              :class="{ exists: getTypesFromMap(itemX, itemY).length != 0 }"
+            >
+              {{ getTypesFromMap(itemX, itemY).length }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 import gql from 'graphql-tag';
 import Query from '../../../database/query';
+import Slider from '../../forms/Slider.vue';
+import LabeledProperty from '../../display/LabeledProperty.vue';
+import RequestBuffer from '../../../models/request-buffer';
 export default {
+  components: { Slider, LabeledProperty },
   name: 'YearMintTablePage',
   created: function () {
     this.fetchTypes();
+
+    this.pinXBuffer = new RequestBuffer(25, true);
+    this.pinYBuffer = new RequestBuffer(25, true);
   },
   data: function () {
     return {
@@ -68,9 +98,19 @@ export default {
       map: new Map(),
       plainValues: ['yearOfMint'],
       nameObjects: ['mint', 'material', 'nominal'],
+      scale: 1,
+      pinXBuffer: null,
+      pinYBuffer: null,
     };
   },
   methods: {
+    updateScale(event) {
+      this.scale = parseFloat(event.currentTarget.value);
+      this.$refs.table.style.transformOrigin = 'top left';
+      this.$refs.table.style.transform = `scale(${this.scale})`;
+
+      this.pinTableHeaders();
+    },
     async fetchTypes() {
       let page = 0;
       const requestSize = 100;
@@ -80,7 +120,9 @@ export default {
         while (!done) {
           const query = gql`
             {
-              coinType(pagination: { count: ${requestSize}, page: ${page} }) {
+              coinType(
+                pagination: { count: ${requestSize}, page: ${page} },
+                filters: {excludeFromTypeCatalogue: false}) {
                 types {
                   ${this.getQuery(this.x)}
                    ${this.getQuery(this.y)}
@@ -124,6 +166,29 @@ export default {
     yChanged(event) {
       this.y = event.target.value;
       this.fetchTypes();
+    },
+    pinTableHeaders(event) {
+      const scrollLeft = this.$refs.viewport.scrollLeft;
+
+      this.pinXBuffer.update(scrollLeft, (value) => {
+        var scaleX =
+          this.tableHead.getBoundingClientRect().width /
+          this.tableHead.offsetWidth;
+        this.tableFirstColumn.forEach((td) => {
+          td.style.left = `${value / scaleX}px`;
+        });
+      });
+
+      const scrollTop = this.$refs.viewport.scrollTop;
+
+      this.pinYBuffer.update(scrollTop, (value) => {
+        var scaleX =
+          this.tableHead.getBoundingClientRect().width /
+          this.tableHead.offsetWidth;
+        this.tableHead.style.top = `${value / scaleX}px`;
+
+        console.log(this.$refs.table.style.transform);
+      });
     },
     typeByMintAndYear(mint, year) {
       return this.map[mint].has(year.toString());
@@ -189,29 +254,58 @@ export default {
     yValues: function () {
       return this.labelsFromType(this.y);
     },
+    tableHead: function () {
+      return this.$refs.table.querySelector('thead');
+    },
+    tableFirstColumn: function () {
+      return this.$refs.table.querySelectorAll('td:first-of-type');
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.slider {
+  margin: 10px;
+}
+.page {
+  max-height: 100%;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  // font-size: 10px;
+}
+
+.viewport {
+  overflow: scroll;
+  background-color: white;
+  flex: 1 0 0;
+}
+
+.select-row {
+  display: flex;
+  width: 100%;
+  > * {
+    flex: 1;
+    display: block;
+
+    select {
+      display: block;
+      width: 100%;
+    }
+  }
+}
+
 thead {
-  position: sticky;
+  z-index: 1;
+  position: relative;
   top: 0;
   background-color: rgba(whitesmoke, 0.95);
 }
 
 h1 {
   margin-bottom: 1em;
-}
-.page {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-
-  align-items: flex-start;
-  padding: 3em 0;
-
-  // font-size: 10px;
 }
 
 .color-box {
@@ -233,5 +327,11 @@ td {
   width: 50px;
   min-width: 50px;
   height: 30px;
+}
+
+td:first-of-type {
+  position: relative;
+  left: 0;
+  background-color: rgba(whitesmoke, 0.95);
 }
 </style>
