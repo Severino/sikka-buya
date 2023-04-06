a1f37d6b2728e28fde643326b04a5388b6a11949
Severino <severin.opel@outlook.com>
2023-04-06 16:02:10 +0200

    other persons now use shortName in typeview

--- frontend/src/components/Person/PersonList.vue
+++ frontend/src/components/Person/PersonList.vue
@@ -4,12 +4,15 @@
       missingText
     }}</span>
     <ul v-else-if="Array.isArray(value)">
-      <li v-for="(item, index) of value" :key="`person-${index}-${item.id}`">
-        {{ item.name }}
+      <li
+        v-for="(item, index) of value"
+        :key="`person-${index}-${item.id}`"
+      >
+        {{ cascadeName(item) }}
       </li>
     </ul>
     <ul v-else>
-      <li>{{ value.name }}</li>
+      <li>{{ cascadeName(value) }}</li>
     </ul>
   </div>
 </template>
@@ -19,6 +22,27 @@ import StringUtils from '../../utils/StringUtils';
 export default {
   props: {
     value: [Array, Object, null],
+    // A list of properties to use as the name of the person
+    // e.g. if the person has no name, the next property will be used
+    targetProps: {
+      type: Array,
+      default: () => ['name'],
+    }
+  },
+  methods: {
+    cascadeName(person) {
+      let name = null
+      let i = 0
+      while (name == null && i < this.targetProps.length) {
+        
+        console.log(this.targetProps[i], person[this.targetProps[i]])
+        if (person[this.targetProps[i]] != null) {
+          name = person[this.targetProps[i]]
+        }
+        i++
+      }
+      return name
+    }
   },
   computed: {
     missingText() {
