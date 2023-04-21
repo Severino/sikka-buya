6292f2919f4636319525933a5d39ee5a0114bac2
Severino <severin.opel@outlook.com>
2023-04-21 17:18:31 +0200

    CMS data from backup now moved to production

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,11 +1,13 @@
 #!/bash/bash
 echo "This script will:\n1. Build the vue app.\n2. Remove existing project in 'www/html'.\n3.Copy new project to 'www/html'."
-echo "[1/4] Build frontend vue app"
+echo "[1/5] Build frontend vue app"
 npm run build
-echo "[2/4] Remove last backup"
+echo "[2/5] Remove last backup"
 sudo rm /var/www/html_backup/* -r
-echo "[3/4] Move old version to backup folder"
+echo "[3/5] Move old version to backup folder"
 sudo mv /var/www/html/* /var/www/html_backup/
-echo "[4/4] Move new version to webroot"
+echo "[4/5] Move new version to webroot"
 sudo mv ~/sikka-buya/frontend/dist/* /var/www/html/
+echo "[5/5] Copy cms data from backup to production folder"
+sudo cp /var/www/html_backup/data/cms/* /var/www/html/data/cms/
 echo "Done! Thanks for deploying a new version of sikka:buya"
