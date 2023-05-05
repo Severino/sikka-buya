0fbe30f00be2b74e1e9e9fa20e9498f97bced5de
Severino <severin.opel@outlook.com>
2023-05-05 17:14:42 +0200

    Fixed env

--- test/actions-test.js
+++ test/actions-test.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+
+
+const pgp = require("pg-promise")
+
+const db = pgp({
+    user: "postgres",
+    host: "postgres",
+    database: "postgres",
+    password: "postgres",
+    port: 5432,
+})
+
+db.none(`CREATE TABLE test (text TEXT)`);
+db.none(`INSERT INTO test VALUES ('Hello World')`);
+const result = db.manyOrNone(`SELECT * FROM test`)
+
+console.log(result)
