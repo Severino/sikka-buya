11fc5c1e9287c8f89a5d099d0f8f8d08ff6c50c5
Severino <severin.opel@outlook.com>
2023-05-10 18:24:09 +0200

    Try using .env

--- test/actions-test.js
+++ test/actions-test.js
@@ -3,15 +3,15 @@ require("dotenv").config()
 const pgp = require("pg-promise")({})
 
 const db = pgp({
-    user: "postgres",
-    host: "postgres",
-    database: "postgres",
-    password: "postgres",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.POSTGRES_HOST || process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.POSTGRES_PORT || process.env.DB_PORT,
 })
 
 async function main() {
-
+    await db.none(`DROP TABLE IF EXISTS test`);
     await db.none(`CREATE TABLE test (text TEXT)`);
     await db.none(`INSERT INTO test VALUES ('Hello World')`);
     const result = await db.manyOrNone(`SELECT * FROM test`)
